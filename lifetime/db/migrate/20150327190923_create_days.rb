277c6465f14b2e5deff6bfb26cc11ffb7cba112c
Catie Kennedy <merida1592@gmail.com>
2015-03-27 12:09:51 -0700

    add day model and controller

---
+++ lifetime/db/migrate/20150327190923_create_days.rb
@@ -0,0 +1,11 @@
+class CreateDays < ActiveRecord::Migration
+  def change
+    create_table :days do |t|
+      t.references :user, index: true
+      t.references :activity, index: true
+      t.datetime :date
+
+      t.timestamps
+    end
+  end
+end
---
+++ lifetime/spec/factories/days.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :day do
+    user nil
+activity nil
+date "2015-03-27 12:09:23"
+  end
+
+end
