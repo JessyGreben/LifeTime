6171a805f62138aa1b6744fbe4432e035de17cae
Catie Kennedy <merida1592@gmail.com>
2015-03-27 12:51:47 -0700

    add things to seed

--- lifetime/db/migrate/20150327185654_create_users.rb
+++ lifetime/db/migrate/20150327185654_create_users.rb
@@ -5,11 +5,11 @@ class CreateUsers < ActiveRecord::Migration
       t.string :lastname
       t.integer :jid
       t.integer :age
-      t.integer :life_expectancy
+      t.decimal :life_expectancy
       t.integer :life_gained_lost
-      t.string :gender
+      t.boolean :gender
       t.integer :weight_kgs
-      t.integer :height_meters
+      t.decimal :height_meters
       t.string :image
 
       t.timestamps
--- lifetime/db/seeds.rb
+++ lifetime/db/seeds.rb
@@ -5,5 +5,39 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-require 'faker'
 
+#create users
+5.times do
+	User.create!(
+		firstname: Faker::Name.firstname,
+		lastname: Faker::Name.lastname,
+		jid: 1 + rand(10),
+		age: 18 + rand(50),
+		life_expectancy: 2.46E9,
+		life_gained_lost: rand(-86400..86400),
+		gender: [true, false].sample,
+		weight_kgs: rand(45..115),
+		height_meters: (1.25..2.5),
+		image: Faker::Avatar.image
+	)
+end
+
+
+num = (1..100).to_a
+
+#create days
+100.times do
+	Day.create!(
+		user_id: 1,
+		activity_id: num.shift,
+		date: Faker::Time.between(300.days.ago, Time.now)
+	)
+
+end
+
+#create activities
+100.times do
+	Activity.create!(
+		steps: rand(600..10000)
+	)
+end
