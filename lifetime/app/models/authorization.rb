0634caf100a5b90e94e488113db7f79150528115
Jessica Grebenschikov & Rebecca Holzschuh <pair+jessygreben.rebeccacaroline@devbootcamp.com>
2015-03-28 12:21:37 -0700

    get data from api call

--- lifetime/app/models/authorization.rb
+++ lifetime/app/models/authorization.rb
@@ -1,3 +1,6 @@
 class Authorization < ActiveRecord::Base
   belongs_to :user
+
+
+  
 end
--- lifetime/db/schema.rb
+++ lifetime/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150328001113) do
+ActiveRecord::Schema.define(version: 20150328034752) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,7 +48,6 @@ ActiveRecord::Schema.define(version: 20150328001113) do
   create_table "users", force: true do |t|
     t.string   "firstname"
     t.string   "lastname"
-    t.integer  "jid"
     t.integer  "age"
     t.decimal  "life_expectancy"
     t.integer  "life_gained_lost"
