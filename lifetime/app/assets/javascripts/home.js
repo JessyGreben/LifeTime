77ababe268ebb39bab4e841032beb4a25687d479
yaysunshine <merida1592@gmail.com>
2015-03-29 11:33:37 -0700

    lifetime di appearing on scroll

--- lifetime/app/assets/javascripts/home.js
+++ lifetime/app/assets/javascripts/home.js
@@ -1,11 +1,26 @@
 $(document).ready(function() {
 
-  $('#lifetime').on('click', function(event){
-    event.preventDefault();
-    $('#lifetime').each(function(){
-      $(this).show();
-    });
+  // $('#lifetime').on('click', function(event){
+  //   event.preventDefault();
+  //   $('#lifetime').each(function(){
+  //     $("<div />", {
+  //       text: "I'm new here."
+  //     }).prependTo('#lifetime');
+  //   });
+  // });
 
+  // $(window).bind('scroll', function(){
+  //   $('#lifetime').css({ opacity: 0.5 })
+  // });
+
+  // $(window).scroll(function(){
+  //     var fromtop = $(document).scrollTop();
+  //     $('#lifetime').css({opacity: 100-fromtop});
+  // });
+
+  $(document).on('scroll', function (e) {
+      $('#lifetime').css('opacity', ($(document).scrollTop() / 500));
   });
+
 
 });
