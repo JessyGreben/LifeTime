b5618c0c46f944e7ed471c93e168061fd62b2185
yaysunshine <merida1592@gmail.com>
2015-03-28 21:04:39 -0700

    saturday night push, working on making lifetime animate, front page looking good

--- lifetime/app/assets/javascripts/home.js
+++ lifetime/app/assets/javascripts/home.js
@@ -1,12 +1,11 @@
 $(document).ready(function() {
 
-  $('#lifetime').scroll(function(event){
+  $('#lifetime').on('click', function(event){
     event.preventDefault();
-    console.log(this);
     $('#lifetime').each(function(){
       $(this).show();
     });
 
-  })
+  });
 
 });
