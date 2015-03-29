dbb898bac592d5563561f6d1a3afcb7600f0bfc6
Jessica Grebenschikov & Rebecca Holzschuh <pair+jessygreben.rebeccacaroline@devbootcamp.com>
2015-03-29 16:20:24 -0700

    user model methods added for height, weight, gender

--- lifetime/app/models/user.rb
+++ lifetime/app/models/user.rb
@@ -2,5 +2,29 @@ class User < ActiveRecord::Base
 	has_many :days, dependent: :destroy
 	has_many :activities, through: :days, dependent: :destroy
 	has_one :authorization, dependent: :destroy
+
+	def weight
+	  lbs = self.weight_kgs * 2.2
+	  lbs.round
+	end
+
+	def height
+	  inches_height = self.height_meters * 39
+	  rounded_inches = inches_height.round
+	  feet_and_inches(rounded_inches)
+	end
+
+	def feet_and_inches(inches)
+	  feet_inches = inches.divmod(12)
+	  puts "#{feet_inches[0]}'#{feet_inches[1]}\""
+	end
+
+	def set_gender
+	  if self.gender == true
+	  	return "Female"
+	  else
+	  	return "Male"
+	  end 
+	end
 end
 
