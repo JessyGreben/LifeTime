770e856ca0996acefc592785bd58987db44370f5
Jessica Grebenschikov & Rebecca Holzschuh <pair+jessygreben.rebeccacaroline@devbootcamp.com>
2015-03-30 12:30:46 -0700

    add logout functionality

--- lifetime/app/controllers/sessions_controller.rb
+++ lifetime/app/controllers/sessions_controller.rb
@@ -52,10 +52,10 @@ class SessionsController < ApplicationController
   # def failure
   # end
 
-  # def destroy
-  # 	session[:id] = nil
-  # 	redirect_to root_path  
-  # end
+  def destroy
+  	session[:id] = nil
+  	redirect_to root_path  
+  end
 
 end
 
